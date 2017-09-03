8bccabea1b28e9e74a241a5a2a577cd9e8b24c12
devendervb12 <info@s-max.in>
2017-09-03 09:38:23 +0530

    OData service inclusion in Manifest

--- WebContent/view/Page1.controller.js
+++ WebContent/view/Page1.controller.js
@@ -1,5 +1,14 @@
 sap.ui.controller("view.Page1", {
 
+	/*onInit : function(){
+		
+		
+		var url = "proxy/http/122.180.87.238:8000/sap/opu/odata/SAP/ZGW_BATCH23_PRODUCT01_SRV/";
+		var oModel = new sap.ui.model.odata.v2.ODataModel(url);
+		this.getView().setModel(oModel);
+		
+	},*/
+	
 	gotoPage2 : function(){
 		// navigate to page 2
 		
