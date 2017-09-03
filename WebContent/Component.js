24589c5b9b05db4413e14144d4bf0710f62f8d66
devendervb12 <info@s-max.in>
2017-09-03 08:28:29 +0530

    Router changes

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,15 +1,40 @@
-
 sap.ui.core.UIComponent.extend("smax.batch2402.Component", {
 	metadata : {
-		rootView : "view.App"		
+		rootView : "view.App",
+		routing : {
+			config : {
+				routerClass : "sap.m.routing.Router",
+				controlId : "idApp",
+				controlAggregation : "pages",
+				viewType : "XML"
+			},
+			routes : [
+				{
+					pattern : "",
+					name : "firstPage",
+					viewName : "view.Page1"
+				
+				},
+				{
+					pattern : "p2",
+					name : "secondPage",
+					viewName : "view.Page2"
+				}, 
+				{
+					pattern : "p3",
+					name : "thirdPage",
+					viewName : "view.Page3"
+				}
+			]
+		}
 	},
-	
-	
-	/*createContent : function(){
-		oApp = sap.ui.xmlview({
-			viewName : "view.App"
-		});
+	//SAPUI5 Router
+	init : function(){
 		
-		return oApp;
-	}*/
+		// intializing smax.batch2402 component with UIComponent, get get Router
+		sap.ui.core.UIComponent.prototype.init.apply(this);
+		
+		// initlaize the router
+		this.getRouter().initialize();
+	}
 })
