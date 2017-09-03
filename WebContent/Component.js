8ec2c9c5437cb23a45626f5c4afd6ac3cf30c816
devendervb12 <info@s-max.in>
2017-09-03 07:45:36 +0530

    initial commit

---
+++ WebContent/Component.js
@@ -0,0 +1,15 @@
+
+sap.ui.core.UIComponent.extend("smax.batch2402.Component", {
+	metadata : {
+		rootView : "view.App"		
+	},
+	
+	
+	/*createContent : function(){
+		oApp = sap.ui.xmlview({
+			viewName : "view.App"
+		});
+		
+		return oApp;
+	}*/
+})
